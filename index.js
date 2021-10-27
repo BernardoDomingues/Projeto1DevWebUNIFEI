1bd8ae3c84e0513408bca50a46c1b72a69f5b65c
Bernardo Domingues <71652591+BernardoDomingues@users.noreply.github.com>
2021-10-27 13:31:50 -0300

    primeiras configurações do servidor

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('testando');
+process.title = "MyWebServer";
+var args = process.argv,
+  port = args[2] || 7070,
+  webServer = require("./server");
+
+webServer.listen(port, function () {
+  console.log("Server started at port " + port);
+});
