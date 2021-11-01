12573f37cc0176dbb61469440da8db9406030293
Bernardo Domingues <71652591+BernardoDomingues@users.noreply.github.com>
2021-11-01 12:11:41 -0300

    cria base de busca de filmes

--- static/index.js
+++ static/index.js
@@ -1,5 +1,38 @@
-const searchMovie = () => {
-  alert("To aqui");
+const searchMovie = async (value) => {
+  const movieApiKey = "k_xmg5bz3v";
+  fetch(`https://imdb-api.com/en/API/SearchMovie/${movieApiKey}/${value}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
 };
 
-document.getElementById("searchButton").addEventListener("click", searchMovie);
+const validadeSearch = () => {
+  const contentSearchBox = document
+    .getElementById("searchBox")
+    .value.toLowerCase();
+  if (contentSearchBox === "") {
+    alert("Nada a Pesquisar");
+  } else {
+    searchMovie(contentSearchBox);
+  }
+};
+
+const validadeKeyPressed = () => {
+  if (event.key === "Enter") {
+    validadeSearch();
+  }
+};
+
+document
+  .getElementById("searchButton")
+  .addEventListener("click", validadeSearch);
+document.addEventListener("keypress", validadeKeyPressed, false);
+
+// ["click", "keypress"].forEach((evt) =>
+//   document
+//     .getElementById("searchButton")
+//     .addEventListener(evt, searchMovie, false)
+// );
